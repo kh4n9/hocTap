66d179365596022a593a12c7c26c44290f7a3ca2
unknown <mkhoangvip@gmail.com>
2023-03-14 10:47:06 +0700

    add DEMO3 class

---
+++ ltOOP/DEMO3.java
@@ -0,0 +1,9 @@
+public class DEMO3 {
+    public static void main(String[] args) {
+        DSHOCSINH danhsach = new DSHOCSINH();
+        danhsach.nhapDS();
+        danhsach.xuatDS();
+        danhsach.sapXep();
+        danhsach.xuatDS();
+    }
+}
