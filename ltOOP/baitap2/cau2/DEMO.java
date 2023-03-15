87923041a399cbb3de296ae9e5162787d00a2e19
unknown <mkhoangvip@gmail.com>
2023-03-15 18:31:27 +0700

    demo final

--- ltOOP/baitap2/cau2/DEMO.java
+++ ltOOP/baitap2/cau2/DEMO.java
@@ -3,12 +3,18 @@ import java.util.Scanner;
 public class DEMO {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int t,m;
         PHANSO p1 = new PHANSO();
         p1.xuatPS();
         p1.nhapPS();
         p1.xuatPS();
         PHANSO p2 = new PHANSO(4, 16);
         p2.xuatPS();
-        PHANSO p3 = new PHANSO(sc.nextInt(),sc.nextInt());
+        System.out.print("nhap tu:");
+        t = sc.nextInt();
+        System.out.print("nhap mau:");
+        m = sc.nextInt();
+        PHANSO p3 = new PHANSO(t, m);
+        p3.xuatPS();
     }
 }
